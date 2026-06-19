8b4b91d15e526fbbdafe897d2b6c381e58769ddb
UNIFED - PROBATUM <info.momentoeficaz@gmail.com>
2026-06-19 18:22:57 +0100

    Update init__.py

--- core/init__.py
+++ core/init__.py
@@ -1,1 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+ZEPHYR - Core Module
+Zero Entropy Fingerprint Hybrid Engine for Resolution
+Versão: 3.0.0
+"""
 
+from .forensic_engine import ForensicEngine
+from .image_processor import ImageProcessor
+from .minutiae_extractor import MinutiaeExtractor
+from .matching_engine import MatchingEngine
+from .xai_explainer import XAIExplainer
+
+__all__ = [
+    'ForensicEngine',
+    'ImageProcessor',
+    'MinutiaeExtractor',
+    'MatchingEngine',
+    'XAIExplainer'
+]
